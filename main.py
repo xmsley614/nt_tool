1fec0a8a932c2e22f8b690f7a340112caead4f32
Chong <chong.teng@bc.edu>
2023-02-11 16:01:29 -0500

    fix main.py

--- main.py
+++ main.py
@@ -2,29 +2,9 @@ import time
 from datetime import datetime
 from parser import convert_response, results_to_excel, results_to_dash_table
 from searcher import Searcher
-from web import create_dash_app
+from web import DashApp
 
 if __name__ == '__main__':
-    results = []
-    max_stops = 1
-    origins = ['BOS']
-    destinations = ['PVG']
-    dates = [
-        '2023-02-11',
-        # '2023-02-12',
-        # '2023-02-13',
-        # '2023-02-14',
-    ]
-    sc = Searcher()
-    for ori in origins:
-        for des in destinations:
-            for date in dates:
-                print(f'search for {ori} to {des} on {date} @ {datetime.now().strftime("%H:%M:%S")}')
-                response = sc.search_for(ori, des, date)
-                v1 = convert_response(response)
-                results.extend(v1)
-                # 搜索量大返回异常时，加上延迟
-                # time.sleep(5)
-    # results_to_excel(results, max_stops=max_stops)
-    app = create_dash_app(results)
-    app.run_server(debug=True)
+
+    app = DashApp()
+    app.dash_app.run(debug=True)
