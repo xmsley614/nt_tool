407344c1b0909c84da95d4992b7123df04e31f6a
mileshuang32 <mileshuang32@gmail.com>
2023-02-11 14:29:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from parser import convert_response, results_to_excel
 from searcher import Searcher
+
+
+def date_range(start_date,future_date):
+    date_list = []
+    start_dt = start_date
+    end_dt = future_date
+    for n in range(int((end_dt - start_dt).days)+1):
+        d = start_dt + timedelta(n)
+        date_list.append(d.strftime('%Y-%m-%d'))
+    return date_list
+
+
 
 if __name__ == '__main__':
     results = []
     max_stops = 1
-    origins = ['BOS', 'ORD']
+    origins = ['BOS','ORD']
     destinations = ['FRA']
-    dates = [
-        '2023-02-11',
-        # '2023-02-12',
-        # '2023-02-13',
-        # '2023-02-14',
-    ]
+    start_dt=datetime.strptime('2023-05-10', '%Y-%m-%d')
+    end_dt = datetime.strptime('2023-05-15', '%Y-%m-%d')
+    dates=date_range(start_dt,end_dt)
+
     sc = Searcher()
     for ori in origins:
         for des in destinations:
