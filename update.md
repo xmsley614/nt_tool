c2a6fdb5f19cf1456b2a668b2dd3e02f114279bd
yshenfab <287049745@qq.com>
2023-04-18 19:06:53 +0800

    added some features

--- update.md
+++ update.md
@@ -1,17 +1,24 @@
+2023-04-18
+
+1. Add sound notification.
+2. Get queries from a csv file, a query includes origin, destination, date
+   range, and other options for filtering. Multiple queires are run as multi-processes.
+
 2023-04-13
+
 1. Add DL engine support. DL engine will only return first 20 results.
 2. Use some way to filter results with the extremely high requirement of miles of AA and DL.
 3. Minor bugs fixed.
 4. Add connection time showing with from_to field.
 
-
 2023-03-26
-1. Update web client.
-2. Remove some codes in use_aa.py since AA engine can not search Premium_Economy cabin. Instead, you can use price 
-   filter.
-3. Re-arrange the structs of classes. 
 
+1. Update web client.
+2. Remove some codes in use_aa.py since AA engine can not search Premium_Economy cabin. Instead, you can use price
+   filter.
+3. Re-arrange the structs of classes.
 
 2023-03-20
+
 1. Add airbound filter.
 2. Remove useless parameter and vars.
--- utils.py
+++ utils.py
@@ -1,14 +1,50 @@
 import datetime
-from datetime import timedelta
+import os
+import time
+from datetime import datetime, timedelta
 from typing import List
-from datetime import datetime
+
+import chime
+import psutil
 
 
-def date_range(start_date:str, end_date:str) ->List[str]:
-    start_dt = datetime.strptime(start_date, '%Y-%m-%d')
-    end_dt = datetime.strptime(end_date, '%Y-%m-%d')
+def date_range(start_date: str, end_date: str) -> List[str]:
+    start_dt = datetime.strptime(start_date, "%Y-%m-%d")
+    end_dt = datetime.strptime(end_date, "%Y-%m-%d")
     date_list = []
     for n in range(int((end_dt - start_dt).days) + 1):
         d = start_dt + timedelta(n)
-        date_list.append(d.strftime('%Y-%m-%d'))
+        date_list.append(d.strftime("%Y-%m-%d"))
     return date_list
+
+
+def is_script_running(script_name):
+    """a daemon program to check if python script is running
+
+    :arg1: script name, "main.py"
+    :returns: bool
+
+    """
+    for q in psutil.process_iter():
+        if q.name().startswith("python"):
+            if (
+                len(q.cmdline()) > 1
+                and script_name in q.cmdline()[1]
+                and q.pid != os.getpid()
+            ):
+                return True
+    return False
+
+
+def play_success_sound(num):
+    for _ in range(num):
+        chime.success()
+        time.sleep(1)
+
+
+def play_error_sound(num):
+    chime.theme("zelda")
+    #  while True:
+    for _ in range(num):
+        chime.error()
+        time.sleep(1)
